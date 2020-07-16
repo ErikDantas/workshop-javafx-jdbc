11d2a875cce915d70624477eac2858bdc04c7acf
ErikDantas <jose.erikdantas@outlook.com>
2020-07-16 18:37:22 -0300

    Modified MainView Design

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,8 +2,8 @@ package application;
 	
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 
@@ -12,8 +12,12 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-			Parent parent = loader.load();
-			Scene mainScene = new Scene(parent);
+			ScrollPane scroolpane = loader.load();
+			
+			scroolpane.setFitToHeight(true);
+			scroolpane.setFitToWidth(true);
+			
+			Scene mainScene = new Scene(scroolpane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
